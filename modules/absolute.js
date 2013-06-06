c29e110f83a378343e3454330af2324b963c4d11
Chan <dulithaz@gmail.com>
2013-06-06 13:27:54 +0530

    Made changes for view and control. If view is available without a controller it will directly go to the view. If a controller is available it will execute the controller without the view. If both of them are unavailable we send a 404 error

--- modules/absolute.js
+++ modules/absolute.js
@@ -1,5 +1,5 @@
-var Handle = require("handlebars").Handlebars;
-var absolute = (function () {
+var Handle = require("/modules/handlebars.js").Handlebars;
+var mvc = (function () {
 	var configs= {
 		SERVER_URL: "/",
 		ENGINE: "hbs"
@@ -56,7 +56,37 @@ var absolute = (function () {
 		}
 	}
 	
-
+	function registerHelpers(){
+		// Handlebars.registerHelper('include', function (contexts) {
+		// 		Handle.compile('')
+		// 		return new Handlebars.SafeString(html);
+		// 	});
+		// From Caramel core/caramel/scripts/caramel.handlebars.js
+		// Handlebars.registerHelper('include', function (contexts) {
+		// 		        var i, type,
+		// 		            length = contexts ? contexts.length : 0,
+		// 		            html = '';
+		// 		        if (log.isDebugEnabled()) {
+		// 		            log.debug('Including : ' + stringify(contexts));
+		// 		        }
+		// 		        if (length == 0) {
+		// 		            return html;
+		// 		        }
+		// 		        type = typeof contexts;
+		// 		        if (contexts instanceof Array) {
+		// 		            for (i = 0; i < length; i++) {
+		// 		                html += renderData(contexts[i]);
+		// 		            }
+		// 		        } else if (contexts instanceof String || type === 'string' ||
+		// 		            contexts instanceof Number || type === 'number' ||
+		// 		            contexts instanceof Boolean || type === 'boolean') {
+		// 		            html = contexts.toString();
+		// 		        } else {
+		// 		            html = renderData(contexts);
+		// 		        }
+		// 		        return new Handlebars.SafeString(html);
+		// 		 });
+	}
 	
 	//If the path has a . return true
 	function isAsset(path){
@@ -90,6 +120,7 @@ var absolute = (function () {
         route: function (req) {
 			var reqURL = req.getRequestURI();
 			var pageURL = reqURL.replace(configs.SERVER_URL, '');
+			//Ignore the specified URIs
 			for (var i=0; i < configs.IGNORE.length; i++) {
 				if(pageURL==configs.IGNORE[i]){
 					include(pageURL);
@@ -98,31 +129,59 @@ var absolute = (function () {
 			};
 			log.info("Request url: "+reqURL);
 			log.info("Page url: "+pageURL);
+			
 			var pageParams = pageURL.split('/');
 			//Send the last part of the uri 
+			//Routing assets
 			if(isAsset(pageParams[pageParams.length-1])){
 				routeAsset(pageURL);
 				return;
 			}
+			
 			var controller = pageParams[0];
 			var view = "index";
 			if(pageParams.length>1 && pageParams[1]!=''){
 				view = pageParams[1];	
 			}
+			var viewName = view;
+			view = view+"."+configs.ENGINE;
+			log.info("View "+ view);
+			
+			//App controller
 			var appController;
 			if(isExists('/controller/app.js')){
 				appController =require('/controller/app.js');
 			}
-			var context = require('/controller/'+controller+".js")[view](appController);
-			view = view+"."+configs.ENGINE;
 			
-			log.info("View "+ view);
-			log.info('/views/'+controller+"/"+view);
-			log.info(Handle.partials);
-			var template = Handle.compile(getResource('/views/'+controller+"/"+view));
-			var b = template(context);
-			var layout = Handle.compile(getResource("/pages/"+context.layout+".hbs"));
-			print(layout({body:b, partials:context.partials}));
+			//Extracting the template from the view
+			var template;
+			var templateURI = '/views/'+controller+"/"+view;
+			if(isExists(templateURI)){
+				template = Handle.compile(getResource(templateURI));
+			}
+			
+			var context;
+			if(isExists('/controller/'+controller+".js") && require('/controller/'+controller+".js")[viewName] !=undefined){
+				context = require('/controller/'+controller+".js")[viewName](appController);
+				log.info("Current context "+context);
+			}		
+			//Extracting the layout from the controller
+			var layout;
+			if(context!=undefined && context.layout!=undefined){
+				layout = Handle.compile(getResource("/pages/"+context.layout+".hbs"));
+			}
+			//If we can't find a controller as well as a view we are sending a 404 error
+			if(template==undefined && context==undefined){
+				response.sendError(404);
+			}else{
+				var b = template(context);
+				if(layout==undefined){
+					//If the controller hasn't specified a layout
+					print(b);
+				}else{
+					print(layout({body:b, partials: context.partials}));
+				}
+			}
         }
     };
 // return module
